b140c8e1e1c91f84e2f81c9bff56224481160a53
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-09-12 15:49:09 -0700

    Create NAPLS2_preprocess_qunex.sh

---
+++ NAPLS_preprocessing/NAPLS2_preprocess_qunex.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# list of qunex commands to complete preprocessing of NAPLS2 minimally preprocessed data
+# recomment do not run script from command line, copy and paste individual commands into terminal
+
