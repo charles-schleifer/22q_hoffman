84d81181da7a0530fd1658cbbd3f4aa42fa29e27
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-09-12 15:46:43 -0700

    Update setup_images.sh

--- NAPLS_preprocessing/setup_images.sh
+++ NAPLS_preprocessing/setup_images.sh
@@ -2,3 +2,10 @@
 
 # script to organize NAPLS2 BOLD results into images directory for further processing
 
+# study directory
+sdir=/u/project/cbearden/data/NAPLS_BOLD/NAPLS2/sessions/S_sessions
+
+for s in ${sdir}/*_S*; do 
+  sesh=$(basename $s)
+  echo $s
+done
