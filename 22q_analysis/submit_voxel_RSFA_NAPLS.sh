b5b60dec54e327483db4cfa729d0117a6dd3a1a1
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-09-13 18:46:01 -0700

    Update submit_voxel_RSFA_NAPLS.sh

--- 22q_analysis/submit_voxel_RSFA_NAPLS.sh
+++ 22q_analysis/submit_voxel_RSFA_NAPLS.sh
@@ -3,15 +3,15 @@
 source /u/local/Modules/default/init/modules.sh
 module load R 
 
-logdir="/u/project/cbearden/data/22q/qunex_studyfolder/processing/logs/manual/"
+logdir="/u/project/cbearden/data/NAPLS_BOLD/NAPLS2/processing/logs/manual/"
 
-# UCLA trio
-sdir="/u/project/cbearden/data/22q/qunex_studyfolder/sessions/"
-for s in ${sdir}/Q_*; do
+# NAPLS2
+sdir="/u/project/cbearden/data/NAPLS_BOLD/NAPLS2/sessions/S_sessions/"
+for s in ${sdir}/*_S*; do
 	sesh=$(basename $s)
 	echo $sesh
-	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWM1d_lpss.dtseries.nii" --bold_name_use="resting"
-	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="resting"
+	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWM1d_lpss.dtseries.nii" --bold_name_use="RESTING"
+	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="RESTING"
 done
 
 
