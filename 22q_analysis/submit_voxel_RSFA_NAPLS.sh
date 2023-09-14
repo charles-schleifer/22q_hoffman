826830d9524cbd2caa7415cf632045f014aa7f74
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-09-13 18:42:39 -0700

    Create submit_voxel_RSFA_NAPLS.sh

---
+++ 22q_analysis/submit_voxel_RSFA_NAPLS.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source /u/local/Modules/default/init/modules.sh
+module load R 
+
+logdir="/u/project/cbearden/data/22q/qunex_studyfolder/processing/logs/manual/"
+
+# UCLA trio
+sdir="/u/project/cbearden/data/22q/qunex_studyfolder/sessions/"
+for s in ${sdir}/Q_*; do
+	sesh=$(basename $s)
+	echo $sesh
+	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_noGSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWM1d_lpss.dtseries.nii" --bold_name_use="resting"
+	qsub -cwd -V -o ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).o -e ${logdir}/${sesh}_voxel_RSFA_GSR.$(date +%s).e -l h_data=16G,h_rt=10:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="resting"
+done
+
+
+# qsub -cwd -V -o /u/project/cbearden/data/22q/qunex_studyfolder/processing/logs/manual//test_voxel_RSFA_GSR.$(date +%s).o -e /u/project/cbearden/data/22q/qunex_studyfolder/processing/logs/manual//test_voxel_RSFA_GSR.$(date +%s).e -l h_data=16G,h_rt=4:00:00  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_voxel_RSFA.sh --sessions_dir="/u/project/cbearden/data/22q/qunex_studyfolder/sessions" --sesh="Q_0001_09242012" --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="resting"
