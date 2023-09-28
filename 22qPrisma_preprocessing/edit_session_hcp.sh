a44f7ee5816b89673d9f382318f48f268b18bd9c
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-09-28 09:55:40 -0700

    Update edit_session_hcp.sh

--- 22qPrisma_preprocessing/edit_session_hcp.sh
+++ 22qPrisma_preprocessing/edit_session_hcp.sh
@@ -26,18 +26,27 @@ echo sessions: $sessions
 
 cd $seshdir
 
+# overwrite text file recording sessions to manually edit
+echo "need manual edit/check of session_hcp.txt" > ${seshdir}/specs/check_t1_t2_for_these_sessions.txt	
+
 for sesh in $sessions; do
 	for tn in T1w T2w; do 
-		n=$(cat ${seshdir}/${sesh}/session_hcp.txt | grep $tn | wc -l)
+ 		# count how many lines have T1w or T2w, find by matching pattern [0-9] : ${tn} because if matching just $tn can overlap multiple protocol names that contain T2 or T1
+		n=$(cat ${seshdir}/${sesh}/session_hcp.txt | grep "[0-9] : ${tn}" | wc -l)
+		# if there are two lines then comment out the second
 		if [[ n -eq 2 ]]; then
 			line=$(cat /u/project/cbearden/data/22qPrisma/qunex_studyfolder/sessions/${sesh}/session_hcp.txt | grep $tn | tail -n1)
 			 sed -i "s/${line}/\#${line}/" ${seshdir}/${sesh}/session_hcp.txt 
 			
-		else
+		# if only one line matches, do nothing, else record subject 
+  		elif [[ n -eq 1 ]]; then
+  			continue
+  		else
 			echo "${sesh} has ${n} ${tn} images, need to manually edit session_hcp.txt"
 			echo "${sesh} : ${tn} : ${n}" >> ${seshdir}/specs/check_t1_t2_for_these_sessions.txt		
 		fi
-	done	
+	done
+ 	# edit subject field from Q to Q_nnnn in session_hcp.txt file
 	subject="Q_$(echo ${sesh} | cut -d "_" -f 2)"
 	sed -i "/subject/c subject: ${subject}" ${seshdir}/${sesh}/session_hcp.txt 
 done
