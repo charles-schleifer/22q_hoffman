c6bbe48bf3cad4a452b110096e7e1679ad893a8a
Charlie Schleifer <135395963+charles-schleifer@users.noreply.github.com>
2023-10-05 18:05:12 -0700

    Update submit_bparcel_fc_napls.sh

--- 22q_analysis/submit_bparcel_fc_napls.sh
+++ 22q_analysis/submit_bparcel_fc_napls.sh
@@ -10,6 +10,6 @@ for s in ${sdir}/04_S*; do
 #for s in ${sdir}/*_S*; do
 	sesh=$(basename $s)
 	echo $sesh
-	#qsub -cwd -V -o ${logdir}/napls_bparc_noGSR.$(date +%s).o -e ${logdir}/napls_bparc_noGSR.$(date +%s).e -l h_data=16G,h_rt=24:00:00,arch=intel*  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_bparcel_fc_individual.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWM1d_lpss.dtseries.nii" --bold_name_use="RESTING"
-	qsub -cwd -V -o ${logdir}/napls_bparc_GSR.$(date +%s).o -e ${logdir}/napls_bparc_GSR.$(date +%s).e -l h_data=16G,h_rt=24:00:00,arch=intel*  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_bparcel_fc_individual.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="RESTING"
+	qsub -cwd -V -o ${logdir}/napls_bparc_noGSR.$(date +%s).o -e ${logdir}/napls_bparc_noGSR.$(date +%s).e -l h_data=16G,h_rt=24:00:00,arch=intel*  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_bparcel_fc_individual.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWM1d_lpss.dtseries.nii" --bold_name_use="RESTING"
+	#qsub -cwd -V -o ${logdir}/napls_bparc_GSR.$(date +%s).o -e ${logdir}/napls_bparc_GSR.$(date +%s).e -l h_data=16G,h_rt=24:00:00,arch=intel*  /u/project/cbearden/data/22q/qunex_studyfolder/analysis/scripts/run_bparcel_fc_individual.sh --sessions_dir=${sdir} --sesh=${sesh} --file_end="_Atlas_s_hpss_res-mVWMWB1d_lpss.dtseries.nii" --bold_name_use="RESTING"
 done
