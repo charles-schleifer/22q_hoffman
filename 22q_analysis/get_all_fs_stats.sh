55fc27be9f516ec108964638df301372f4ed479e
charles-schleifer <charlie.schleifer@gmail.com>
2024-01-03 10:55:05 -0800

    update

--- 22q_analysis/get_all_fs_stats.sh
+++ 22q_analysis/get_all_fs_stats.sh
@@ -4,11 +4,14 @@ basepath="/u/project/cbearden/data/"
 
 paths="22q/qunex_studyfolder/sessions/ 22qPrisma/qunex_studyfolder/sessions/ Enigma/SUNY/qunex_studyfolder/sessions/ Enigma/Rome/qunex_studyfolder/sessions/ Enigma/IoP/qunex_studyfolder/sessions/ NAPLS_BOLD/NAPLS2/sessions/S_sessions/"
 
+opath="/u/home/s/schleife"
+
 for studypath in $paths; do
 	# get sessions as any dir in studypath with letter followed by number
 	seshpaths=${basepath}/${studypath}/*[a-zA-Z]*[0-9]*
 	for spath in $seshpaths; do
 		sesh=$(basename $spath)
+		mkdir -p ${opath}/${sesh}/
 		cp -rv ${basepath}/${studypath}/${sesh}/hcp/${sesh}/T1w/${sesh}/stats ${opath}/${sesh}/
 	done
 done
